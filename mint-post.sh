199fe91c28f6c2b0c738d7767e4e376de801302a
vfalmado <61508951+vfalmado@users.noreply.github.com>
2022-07-23 16:55:45 -0300

    Added ZapZap flatpak

--- mint-post.sh
+++ mint-post.sh
@@ -31,6 +31,7 @@ flatpak install flathub org.kde.kdenlive -y
 flatpak install flathub org.kde.akregator -y
 flatpak install flathub com.todoist.Todoist -y
 flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.rtosta.zapzap -y
 
 # Installing PPAs
 
