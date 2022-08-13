d5fa9f08f00ed5026eb6d027cba977c3fa4f53e4
vfalmado <61508951+vfalmado@users.noreply.github.com>
2022-08-13 17:34:25 -0300

    New programs and minor corrections
    Added Multipass, ClamTK and PhotoGIMP. Minor corrections to the code.

--- mint-post.sh
+++ mint-post.sh
@@ -15,7 +15,7 @@ sudo apt install snapd -y
 
 # Installing Snap packages
 
-sudo snap install authy
+sudo snap install authy multipass
 
 # Installing flatpak packages
 
@@ -27,11 +27,14 @@ flatpak install flathub com.valvesoftware.Steam -y
 flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub org.kde.tellico -y
-flatpak install flathub org.kde.kdenlive -y
 flatpak install flathub org.kde.akregator -y
-flatpak install flathub com.todoist.Todoist -y
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub com.rtosta.zapzap -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub com.gitlab.davem.ClamTk -y
+sudo freshclam
+
+# Downloading PhotoGIMP
+
+wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
 
 # Installing PPAs
 
